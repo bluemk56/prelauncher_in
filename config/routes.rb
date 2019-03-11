47c89a5e8db476821b1dd178c892f69c26361529
joshua.lee@goodmonit.com <joshua.lee@goodmonit.com>
2019-03-11 17:11:49 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  #	get ':controller(/:action(/:id))(.:format)' => 'entries#sign_in'
  # get 'entries/welcome' => 'entries#welcome'
 get 'entries/welcome',to: 'entries#welcome', as: 'welcome'
-
+get 'entries/welcome(/:id)(.:format)', :to => 'entries#sign_in'
   match ':controller(/:action(/:id))(.:format)' , :to => 'controller#action', :via => [:get, :post]
   #    match ':controller(/:action(/:unique))(.:format)' , :to => 'controller#action', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
