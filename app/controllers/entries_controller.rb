cd83a7d14b9b1a1de8883f52c3728995849af3ab
joshua.lee@goodmonit.com <joshua.lee@goodmonit.com>
2019-03-13 08:01:31 +0900

    design change

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -13,31 +13,29 @@ class EntriesController < ApplicationController
 #	  puts	"------------------------------>>>>>>> #{ref}, #{valid} "
 #  	unless @name.blank?    #false, Entry.find_b(name: @name).nil? true,-->
   puts " 1.5 ----------------------------------->#{@name.blank?}  #{Entry.find_by(name: @name).nil?} "
-	if !(@name.blank?) && (Entry.find_by(name: @name).nil?) 
-    puts " 2 ------------------------------------> #{@name}"
-    @unique_hex= SecureRandom.hex(10);
-  	@entry= Entry.create({:name =>@name, :unique => @unique_hex})
-    puts " 3 ------------------------------------> #{@name}"
-		
-    if !(valid) && !(ref_e) 		
-			obj_ref=Entry.find_by(unique: ref);
-      puts "------------------------------>>>>>>>>>#{obj_ref.name}"
-			obj_ref.friend ||= 0
-			obj_ref.friend= obj_ref.friend+1;
-			obj_ref.update({:friend => obj_ref.friend});
-  		puts "------------------------------>>>>>>>>>#{obj_ref.friend}"
-  	end
-          redirect_to(:action => 'welcome')
-#  		@entries= Entry.all
-#  		render "entries/ongoing"
-
+	if !(@name.blank?)
+    if (Entry.find_by(name: @name).nil?) 
+      @unique_hex= SecureRandom.hex(10);
+    	@entry= Entry.create({:name =>@name, :unique => @unique_hex})	
+     
+      if !(valid) && !(ref_e) 		
+	  		obj_ref=Entry.find_by(unique: ref);
+      	obj_ref.friend ||= 0
+	  		obj_ref.friend= obj_ref.friend+1;
+	  		obj_ref.update({:friend => obj_ref.friend});
+  		end
+    end
+    redirect_to(:action => 'welcome', :name => @name)
   end
+end
 
-  end
-#  def ongoing
-#  	@entries_1= Entry.all
-# end
+
 def welcome
+  name= params[:name];
+  full_numb_ref= 20;
+  @user= Entry.find_by(name: name)
+  @percentage= @user.friend*(100/full_numb_ref);
+puts "----------------------11-------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#{@user.friend}*5  #{@percentage}"
 	@entries1= Entry.all
 
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,7 @@
 class Entry < ApplicationRecord
+	after_initialize :set_default
+	def set_default
+		self.friend ||= 0;
+		puts "Entry.friend initializer Running......................."
+	end
 end
