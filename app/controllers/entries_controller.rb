aad88097d6c075e789e10c8a6275050d33bf7726
joshua.lee@goodmonit.com <joshua.lee@goodmonit.com>
2019-03-12 16:47:35 +0900

    initial

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,10 @@
 class EntriesController < ApplicationController
   def sign_in
   	@name= params[:visitor_name]
+    puts "1------------------------------------> #{@name}"
   	ref_e= params[:id].nil?; # true-->nothing in url, false--> something in url
   	ref= params[:id];
+    obj_ref=Entry.find_by(unique: ref);
 	  valid= Entry.find_by(unique: ref).nil? # true==> fake false==> real
 
 #	if  valid= Entry.find_by(unique: ref).nil? 
@@ -10,20 +12,25 @@ class EntriesController < ApplicationController
 #	end
 #	  puts	"------------------------------>>>>>>> #{ref}, #{valid} "
 #  	unless @name.blank?    #false, Entry.find_b(name: @name).nil? true,-->
-  
+  puts " 1.5 ----------------------------------->#{@name.blank?}  #{Entry.find_by(name: @name).nil?} "
 	if !(@name.blank?) && (Entry.find_by(name: @name).nil?) 
+        puts " 2 ------------------------------------> #{@name}"
       @unique_hex= SecureRandom.hex(10);
   		@entry= Entry.create({:name =>@name, :unique => @unique_hex})
+          puts " 3 ------------------------------------> #{@name}"
 		if !(valid) && !(ref_e) 		
 			obj_ref=Entry.find_by(unique: ref);
+              puts "------------------------------>>>>>>>>>#{obj_ref.name}"
 			obj_ref.friend ||= 0
 			obj_ref.friend= obj_ref.friend+1;
 			obj_ref.update({:friend => obj_ref.friend});
   			puts "------------------------------>>>>>>>>>#{obj_ref.friend}"
   		end
+      view_context.link_to("", welcome_path(@unique_hex))
   	end
-  		@entries= Entry.all
+#  		@entries= Entry.all
 #  		render "entries/ongoing"
+
   end
 
 #  def ongoing
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
  #	get ':controller(/:action(/:id))(.:format)' => 'entries#sign_in'
  # get 'entries/welcome' => 'entries#welcome'
-get 'entries/welcome',to: 'entries#welcome', as: 'welcome'
-get 'entries/welcome(/:id)(.:format)', :to => 'entries#sign_in'
+ root 'entries#welcome'
+get 'entries/welcome', to: 'entries#welcome', as: 'welcome'
+
   match ':controller(/:action(/:id))(.:format)' , :to => 'controller#action', :via => [:get, :post]
   #    match ':controller(/:action(/:unique))(.:format)' , :to => 'controller#action', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
