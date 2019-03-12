72bdf0a42fec154f68e2acd54952d9ed62aa9858
joshua.lee@goodmonit.com <joshua.lee@goodmonit.com>
2019-03-13 03:50:43 +0900

    logic_check

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,25 +14,26 @@ class EntriesController < ApplicationController
 #  	unless @name.blank?    #false, Entry.find_b(name: @name).nil? true,-->
   puts " 1.5 ----------------------------------->#{@name.blank?}  #{Entry.find_by(name: @name).nil?} "
 	if !(@name.blank?) && (Entry.find_by(name: @name).nil?) 
-        puts " 2 ------------------------------------> #{@name}"
-      @unique_hex= SecureRandom.hex(10);
-  		@entry= Entry.create({:name =>@name, :unique => @unique_hex})
-          puts " 3 ------------------------------------> #{@name}"
-		if !(valid) && !(ref_e) 		
+    puts " 2 ------------------------------------> #{@name}"
+    @unique_hex= SecureRandom.hex(10);
+  	@entry= Entry.create({:name =>@name, :unique => @unique_hex})
+    puts " 3 ------------------------------------> #{@name}"
+		
+    if !(valid) && !(ref_e) 		
 			obj_ref=Entry.find_by(unique: ref);
-              puts "------------------------------>>>>>>>>>#{obj_ref.name}"
+      puts "------------------------------>>>>>>>>>#{obj_ref.name}"
 			obj_ref.friend ||= 0
 			obj_ref.friend= obj_ref.friend+1;
 			obj_ref.update({:friend => obj_ref.friend});
-  			puts "------------------------------>>>>>>>>>#{obj_ref.friend}"
-  		end
-      view_context.link_to("", welcome_path(@unique_hex))
+  		puts "------------------------------>>>>>>>>>#{obj_ref.friend}"
   	end
+          redirect_to(:action => 'welcome')
 #  		@entries= Entry.all
 #  		render "entries/ongoing"
 
   end
 
+  end
 #  def ongoing
 #  	@entries_1= Entry.all
 # end
