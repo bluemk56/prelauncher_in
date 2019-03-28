334b9e4d2e58d64c0fcf9504c62a5e82565d0fa4
joshua.lee@goodmonit.com <joshua.lee@goodmonit.com>
2019-03-29 03:32:03 +0900

    URL Change

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
  #	get ':controller(/:action(/:id))(.:format)' => 'entries#sign_in'
  # get 'entries/welcome' => 'entries#welcome'
- root 'entries#welcome'
-get 'entries/welcome', to: 'entries#welcome', as: 'welcome'
-
-  match ':controller(/:action(/:id))(.:format)' , :to => 'controller#action', :via => [:get, :post]
+  root 'entries#sign_in'
+  get '/welcome', to: 'entries#welcome', as: 'welcome'
+  get '/:id(.:format)', :to => 'entries#sign_in'
+  post '/', to: 'entries#sign_in', as: 'sign_in'
+  post '/:id(.:format)', to: 'entries#sign_in'
   #    match ':controller(/:action(/:unique))(.:format)' , :to => 'controller#action', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
