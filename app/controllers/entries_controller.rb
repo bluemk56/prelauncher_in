29c31780b4d663000b0bfdd81adea44ca76bcdb2
Vladimir Skrobot <vskrobot@gmail.com>
2019-03-30 00:50:53 -0700

    Rework visited logic

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,32 +10,31 @@ class EntriesController < ApplicationController
 
     puts " 1.5 ----------------------------------->sentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsentsent "
 
-    @visited = false
-    if Entry.find_by(ip: request.remote_ip)
-      @visited = true
-    end
+    @visited = !Entry.find_by(ip: request.remote_ip).nil?
 
-    if !@email.blank? && !@visited
-      if @email.match(URI::MailTo::EMAIL_REGEXP).present?
-        if (Entry.find_by(email: @email).nil?)
-          @unique_hex = SecureRandom.hex(10)
-          UserMailer.signup_confirmation(@email).deliver
-          @entry = Entry.create({:email => @email, :unique => @unique_hex, :ip => request.remote_ip})
+    if @email.present? && @email.match(URI::MailTo::EMAIL_REGEXP).present?
+      entry = Entry.find_by(email: @email)
 
-          if !(valid) && !(ref_e)
-            obj_ref = Entry.find_by(unique: ref)
-            obj_ref.friend ||= 0
-            obj_ref.friend = obj_ref.friend + 1
-            obj_ref.update({:friend => obj_ref.friend})
-            @entry.update({:wrefered => obj_ref.email})
-          end
-        end
+      if @visited && entry
         redirect_to(:action => 'welcome', :email => @email)
       end
-      puts "------------------->1"
-    end
-    puts "----------------->2"
 
+      if !@visited && entry.nil?
+        @unique_hex = SecureRandom.hex(10)
+        UserMailer.signup_confirmation(@email).deliver
+        @entry = Entry.create({:email => @email, :unique => @unique_hex, :ip => request.remote_ip})
+
+        if !(valid) && !(ref_e)
+          obj_ref = Entry.find_by(unique: ref)
+          obj_ref.friend ||= 0
+          obj_ref.friend = obj_ref.friend + 1
+          obj_ref.update({:friend => obj_ref.friend})
+          @entry.update({:wrefered => obj_ref.email})
+        end
+
+        redirect_to(:action => 'welcome', :email => @email)
+      end
+    end
   end
 
 
