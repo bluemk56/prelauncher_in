35275107763a9206c7bf4aabf4d85fd2b20d1dca
joshua.lee@goodmonit.com <joshua.lee@goodmonit.com>
2019-03-30 13:53:06 +0900

    production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'bebefit.io',
+  user_name:            'bluemk56@gmail.com',
+  password:             '1q2w3e4r!!',
+  authentication:       'plain',
+  enable_starttls_auto: true }
 end
